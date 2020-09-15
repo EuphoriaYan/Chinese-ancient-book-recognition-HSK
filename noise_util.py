c10ff65ce7aab14b334c6a76a56d4b3f97a7c031
Euphoria Yan <ysq58000@foxmail.com>
2020-09-15 22:34:15 +0800

    update

--- noise_util.py
+++ noise_util.py
@@ -23,19 +23,25 @@ def add_noise(img, generate_ratio=0.001, generate_size=0.003):
             for j in range(P_noise_y - R, P_noise_y + R):
                 if cal_dis((i, j), (P_noise_x, P_noise_y)) < R:
                     if random.random() < 0.8:
-                        img[j][i] = 0
+                        if random.random() < 0.5:
+                            img[j][i] = 0
+                        else:
+                            img[j][i] = 255
         R = random.randint(1, R_max * 2)
         P_noise_x = random.randint(0, w - 1 - R)
         P_noise_y = random.randint(0, h - 1 - R)
         for i in range(P_noise_x + 1, P_noise_x + R):
             for j in range(P_noise_y + 1, P_noise_y + R):
                 if random.random() < 0.8:
-                    img[j][i] = 0
+                    if random.random() < 0.5:
+                        img[j][i] = 0
+                    else:
+                        img[j][i] = 255
     img = Image.fromarray(img)
     return img
 
 
 if __name__ == '__main__':
-    img = Image.open('data/book_pages/imgs_vertical/book_page_0.jpg')
+    img = Image.open('data/book_pages/imgs_vertical/book_page_1.jpg')
     img = add_noise(img)
     img.show()
