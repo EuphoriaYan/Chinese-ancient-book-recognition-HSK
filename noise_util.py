dc10db81c15d1f8077da23fa055d7b593923ad6c
Euphoria Yan <ysq58000@foxmail.com>
2020-09-15 22:51:53 +0800

    update

--- noise_util.py
+++ noise_util.py
@@ -10,33 +10,47 @@ def cal_dis(pA, pB):
     return math.sqrt((pA[0] - pB[0])**2 + (pA[1] - pB[1])**2)
 
 
-def add_noise(img, generate_ratio=0.001, generate_size=0.003):
+def add_noise(img, generate_ratio=0.003, generate_size=0.004):
     if not isinstance(img, np.ndarray):
         img = np.array(img)
     h, w = img.shape
     R_max = max(3, int(min(h, w) * generate_size))
-    for _ in range(int(h * w * generate_ratio)):
-        R = random.randint(1, R_max)
+    threshold = int(h * w * generate_ratio)
+    random_choice_list = []
+    for i in range(1, R_max + 1):
+        random_choice_list.extend([i] * (R_max - i + 1))
+    cnt = 0
+
+    while True:
+        R = random.choice(random_choice_list)
         P_noise_x = random.randint(R, w - 1 - R)
         P_noise_y = random.randint(R, h - 1 - R)
         for i in range(P_noise_x - R, P_noise_x + R):
             for j in range(P_noise_y - R, P_noise_y + R):
                 if cal_dis((i, j), (P_noise_x, P_noise_y)) < R:
                     if random.random() < 0.8:
-                        if random.random() < 0.5:
-                            img[j][i] = 0
-                        else:
-                            img[j][i] = 255
-        R = random.randint(1, R_max * 2)
+                        img[j][i] = 0 if random.random() < 0.5 else 255
+        cnt += 2 * R
+        if cnt >= threshold:
+            break
+
+    R_max *= 2
+    random_choice_list = []
+    for i in range(1, R_max + 1):
+        random_choice_list.extend([i] * (R_max - i + 1))
+    cnt = 0
+    while True:
+        R = random.choice(random_choice_list)
         P_noise_x = random.randint(0, w - 1 - R)
         P_noise_y = random.randint(0, h - 1 - R)
         for i in range(P_noise_x + 1, P_noise_x + R):
             for j in range(P_noise_y + 1, P_noise_y + R):
                 if random.random() < 0.8:
-                    if random.random() < 0.5:
-                        img[j][i] = 0
-                    else:
-                        img[j][i] = 255
+                    img[j][i] = 0 if random.random() < 0.5 else 255
+        cnt += R
+        if cnt >= threshold:
+            break
+
     img = Image.fromarray(img)
     return img
 
