6d73bc5dd0e8cae997bfbe652f9697a32f6c81f5
Euphoria Yan <ysq58000@foxmail.com>
2020-11-05 19:41:33 +0800

    update

--- data_conversion.py
+++ data_conversion.py
@@ -3,6 +3,7 @@
 
 import os
 import json
+from PIL import Image, ImageDraw
 
 from config import BOOK_PAGE_TAGS_FILE_H, BOOK_PAGE_IMGS_H
 from config import BOOK_PAGE_TAGS_FILE_V, BOOK_PAGE_IMGS_V
@@ -19,6 +20,28 @@ from segment_text_line.data_conversion import main as segment_text_line_main
 from segment_double_line.data_conversion import main as segment_double_line_main
 from recog_with_components.extract_data import main as extract_paths_main
 
+def convert_fake_bookpages_to_ReCTS():
+    os.makedirs('data/fake_bookpages/train/img', exist_ok=True)
+    os.makedirs('data/fake_bookpages/train/gt', exist_ok=True)
+    os.makedirs('data/fake_bookpages/val/img', exist_ok=True)
+    gt_file = 'data/book_pages/book_pages_tags_vertical_3.txt'
+    gt_dict = dict()
+    with open(gt_file, 'r', encoding='utf-8') as fp:
+        for line in fp:
+            line = line.strip().split('\t')
+            gt_dict[line[0]] = json.loads(line[1])
+    img_path = 'data/book_pages/imgs_vertical'
+    for img_file_name in img_path:
+        img_file_path = os.path.join(img_path, img_file_name)
+        img_gt = gt_dict[img_file_name]
+        img = Image.open(img_file_path).convert('RGB')
+        draw = ImageDraw.Draw(img)
+        char_bbox_list = img_gt['char_bbox_list']
+        for bbox in char_bbox_list:
+            draw.rectangle(bbox, outline=(255,0,0), width=3)
+        img.show()
+
+
 if __name__ == '__main__':
     # convert_annotation_crnn(src_list=[(TEXT_LINE_TAGS_FILE_H, TEXT_LINE_IMGS_H)],
     #                         dest_file=CRNN_TEXT_LINE_TAGS_FILE_H)
@@ -28,7 +51,7 @@ if __name__ == '__main__':
     # convert_annotation_ctpn(img_sources=[(BOOK_PAGE_TAGS_FILE_V, BOOK_PAGE_IMGS_V)])
     # convert_annotation_ctpn(tfrecords_dir=BOOK_PAGE_TFRECORDS_V)
 
-    segment_book_page_main()
+    # segment_book_page_main()
     # segment_mix_line_main()
     # segment_text_line_main()
     # segment_double_line_main()
